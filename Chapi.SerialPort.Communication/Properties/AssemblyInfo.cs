d5305eb7b4c5ae4b461681d0994438c36ff3af87
Diego Delgado Peña <ddp.trabajo@gmail.com>
2017-09-15 10:18:48 -0400

    Private members naming.

--- Chapi.SerialPort.Communication/Properties/AssemblyInfo.cs
+++ Chapi.SerialPort.Communication/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("Chapi.SerialPort.Communication")]
+[assembly: AssemblyTitle("Chapi.SerialPortLib.Communication")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Chapi.SerialPort.Communication")]
+[assembly: AssemblyProduct("Chapi.SerialPortLib.Communication")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- Chapi.SerialPort.Communication/SerialPortCom.cs
+++ Chapi.SerialPort.Communication/SerialPortCom.cs
@@ -4,8 +4,7 @@ using System.Diagnostics;
 using System.IO.Ports;
 using NLog;
 
-
-namespace Chapi.SerialPort.Communication
+namespace Chapi.SerialPortLib.Communication
 {
     /// <summary>
     /// Serial Port communication Input/Output
@@ -14,8 +13,24 @@ namespace Chapi.SerialPort.Communication
     {
         #region Private Attributes
 
+        internal static Logger logger = LogManager.GetCurrentClassLogger();
 
+        private SerialPort _serialPort;
+        private string _portName = "";
+        private Parity _parity = Parity.None;
+        private StopBits _stopBits = StopBits.One;
+        private int _baudRate = 9600;
 
+        // Error state variable for Read/write
+        private bool hasReadWriteError = true;
+
+        // Serial Port reader Task
+        private Thread reader;
+        // Serial Port connection watcher
+        private Thread connectionWatcher;
+
+        private object accessLock = new object();
+        private bool disconnectRequested = false;
         #endregion
     }
 }

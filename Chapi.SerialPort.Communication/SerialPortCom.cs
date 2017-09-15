4055bddcf9ab1872b5e6cd551298d89f9ab17f53
Diego Delgado Peña <ddp.trabajo@gmail.com>
2017-09-15 09:51:52 -0400

    Dependencies

--- Chapi.SerialPort.Communication/SerialPortCom.cs
+++ Chapi.SerialPort.Communication/SerialPortCom.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.IO.Ports;
+using NLog;
 
 namespace Chapi.SerialPort.Communication
 {
+    /// <summary>
+    /// Serial Port communication Input/Output
+    /// </summary>
     public class SerialPortCom
     {
+        #region Private Attributes
 
+
+
+        #endregion
     }
 }
